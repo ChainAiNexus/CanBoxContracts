f19b1ebdef36dad17c611a600100845a437b5325
ikaharyanti263872 <159133555+ikaharyanti263872@users.noreply.github.com>
2025-02-24 21:13:13 +0800

    Update stake.rs

--- stake.rs
+++ stake.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Token, Transfer};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("");
 
 #[program]
 pub mod staking {
